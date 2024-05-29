68a01b5ee2dd380a8b69d599930d1b5038feef97
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-05-29 16:14:38 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<template>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+h1{
+  color: rgb(143, 143, 0)
+}
+</style>
